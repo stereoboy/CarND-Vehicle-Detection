dcafac5ace56145e13c05e7af019a3fe1cf70286
Jaeyoung Lee <jylee0121@gmail.com>
2017-05-13 05:28:11 +0900

    implement training process
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

---
+++ train.py
@@ -0,0 +1,146 @@
+import os
+import sys
+import pickle
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import glob
+import time
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
+from sklearn.model_selection import train_test_split
+from feature import single_img_features
+
+# Define a function to return HOG features and visualization
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
+                        vis=False, feature_vec=True):
+    # Call with two outputs if vis==True
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
+                                  visualise=vis, feature_vector=feature_vec)
+        return features, hog_image
+    # Otherwise call with one output
+    else:      
+        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
+                       visualise=vis, feature_vector=feature_vec)
+        return features
+
+# Define a function to extract features from a list of images
+# Have this function call bin_spatial() and color_hist()
+def extract_features(imgs, color_space='RGB', orient=9, spatial_size=(32, 32), hist_bins=32,
+                        pix_per_cell=8, cell_per_block=2, hog_channel=0,
+                        spatial_feat=True, hist_feat=True, hog_feat=True):    
+    # Create a list to append feature vectors to
+    features = []
+    # Iterate through the list of images
+    for file in imgs:
+        # Read in each one by one
+        image = mpimg.imread(file)
+        # Append the new feature vector to the features list
+        feature = single_img_features(image, color_space=color_space, 
+                            spatial_size=spatial_size, hist_bins=hist_bins, 
+                            orient=orient, pix_per_cell=pix_per_cell, 
+                            cell_per_block=cell_per_block, 
+                            hog_channel=hog_channel, spatial_feat=spatial_feat, 
+                            hist_feat=hist_feat, hog_feat=hog_feat)
+        features.append(feature)
+    # Return list of feature vectors
+    return features
+
+def build_filenames(dirs):
+    base_dir = './data/'
+    print('Search files...')
+
+    filenames = []
+    for subdir in dirs:
+        subdirpath = os.path.join(base_dir, subdir)
+        for root, dirs, files in os.walk(subdirpath):
+            print(root)
+            for ext_filter in ['*.png', '*.jpeg']:
+                path = os.path.join(root, ext_filter)
+                print(path)
+                _filenames = glob.glob(path)
+                filenames.extend(_filenames)
+    return filenames
+
+def main():
+    # Divide up into cars and notcars
+    notcar_dirs = ['non-vehicles', 'non-vehicles_smallset']
+    car_dirs = ['vehicles', 'vehicles_smallset']
+
+    
+    cars = build_filenames(car_dirs)
+    notcars = build_filenames(notcar_dirs)
+    print('numbers of car files: {}'.format(len(cars)))
+    print('numbers of notcar files: {}'.format(len(notcars)))
+
+    ### TODO: Tweak these parameters and see how the results change.
+    colorspace = 'YCrCb' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+    hist_bins=32
+    orient = 9
+    pix_per_cell = 8
+    cell_per_block = 2
+    hog_channel = "ALL" # Can be 0, 1, 2, or "ALL"
+
+    t=time.time()
+    car_features = extract_features(cars, color_space=colorspace, orient=orient, 
+                            pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, 
+                            hog_channel=hog_channel)
+    notcar_features = extract_features(notcars, color_space=colorspace, orient=orient, 
+                            pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, 
+                            hog_channel=hog_channel)
+    t2 = time.time()
+    print(round(t2-t, 2), 'Seconds to extract HOG features...')
+    # Create an array stack of feature vectors
+    X = np.vstack((car_features, notcar_features)).astype(np.float64)                        
+    # Fit a per-column scaler
+    X_scaler = StandardScaler().fit(X)
+    # Apply the scaler to X
+    scaled_X = X_scaler.transform(X)
+
+    # Define the labels vector
+    y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+    # Split up data into randomized training and test sets
+    rand_state = np.random.randint(0, 100)
+    X_train, X_test, y_train, y_test = train_test_split(
+        scaled_X, y, test_size=0.2, random_state=rand_state)
+
+    print('Using:',orient,'orientations',pix_per_cell,
+        'pixels per cell and', cell_per_block,'cells per block')
+    print('Feature vector length:', len(X_train[0]))
+    # Use a linear SVC 
+    svc = LinearSVC()
+    # Check the training time for the SVC
+    t=time.time()
+    svc.fit(X_train, y_train)
+    t2 = time.time()
+    print(round(t2-t, 2), 'Seconds to train SVC...')
+    # Check the score of the SVC
+    print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+    # Check the prediction time for a single sample
+    t=time.time()
+    n_predict = 10
+    print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+    print('For these',n_predict, 'labels: ', y_test[0:n_predict])
+    t2 = time.time()
+    print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
+
+    print('save all results into pickle file')
+    dist_pickle = {}
+    dist_pickle["svc"] = svc
+    dist_pickle["scaler"] = X_scaler
+    dist_pickle["orient"] = orient
+    dist_pickle["pix_per_cell"] = pix_per_cell
+    dist_pickle["cell_per_block"] = cell_per_block
+    dist_pickle["hist_bins"] = hist_bins
+    with open("svc_pickle.p", "wb") as f:
+        pickle.dump(dist_pickle, f)
+
+
+if __name__ == '__main__':
+    main()
